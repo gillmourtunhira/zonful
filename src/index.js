b0a503b8cabe8b6066febb4477953f2cd6edc889
gillmourtunhira <gillmourtunhira@gmail.com>
2020-11-24 03:36:39 +0200

    setting up dev-server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './styles.scss';
 
 function component() {
     const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        port: 9000
+    },
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /\.s[ac]ss/i,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ],
+        }],
     },
 };
