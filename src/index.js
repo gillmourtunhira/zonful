cc7bff745502d291b674626d96c95270a8394db8
gillmourtunhira <gillmour8@gmail.com>
2020-11-25 15:45:40 +0200

    heading change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.scss'
 
 
 function App(){
-    return <h1>Hello from React!</h1>
+    return <h1>Hello from Zonful Energy!</h1>
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
