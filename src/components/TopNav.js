1ce1c31b8179ff954613ea7bbbecb016b52e26f0
gillmourtunhira <gillmour8@gmail.com>
2020-11-26 09:56:15 +0200

    separate component files

---
+++ src/components/TopNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TopNav(){
+    return(
+        <React.Fragment>
+            <div class="top__nav"></div>
+            <div class="main__menu"></div>
+        </React.Fragment>
+    );
+}
+
+export default TopNav
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Fragment } from 'react';
 import _ from 'lodash'
 import './styles.scss'
+import TopNav from './components/TopNav'
 
 
 function App(){
     return <h1>Hello from Zonful Energy!</h1>
 }
 
-function TopNav(){
-    return(
-        <div>
-            <div class="top__nav"></div>
-            <div class="main__menu"></div>
-        </div>
-    );
-}
 
 ReactDOM.render(<App />, document.getElementById("app"));
 ReactDOM.render(<TopNav/>, document.querySelector('header'));
