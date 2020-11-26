88b1b8166f448036f3d692cd711ea954c519798f
gillmourtunhira <gillmour8@gmail.com>
2020-11-26 08:23:14 +0200

    header section added

--- src/index.js
+++ src/index.js
@@ -8,4 +8,14 @@ function App(){
     return <h1>Hello from Zonful Energy!</h1>
 }
 
+function TopNav(){
+    return(
+        <div>
+            <div class="top__nav"></div>
+            <div class="main__menu"></div>
+        </div>
+    );
+}
+
 ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<TopNav/>, document.querySelector('header'));
