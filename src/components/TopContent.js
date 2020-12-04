c5332e21bd9044000db6c87d8983f5aa5c3ff961
gillmourtunhira <gillmourtunhira@gmail.com>
2020-12-04 13:35:57 +0200

    changes to the middle tier

--- src/components/TopContent.js
+++ src/components/TopContent.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import slide from '../img/slidemin.png'
+import slide from '../img/slideog.png'
 
-function SlideContent(){
-    return (
-        <React.Fragment>
-            <div className="main__top">
-                <div className="main__top--slider">
-                    <img src={slide} alt=""/>
-                    <div className="main__top--byline">
-                        <h3>Zonful Energy, Solar power to the people</h3>
-                        <span>If you are interested in going solar, we have every solar solution for you <i className="material-icons">call_made</i></span>
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
+function SlideContent() {
+    return ( <
+        React.Fragment >
+        <
+        div className = "main__top" >
+        <
+        div className = "main__top--slider" >
+        <
+        img src = {
+            slide
+        }
+        alt = "" / >
+        <
+        div className = "main__top--byline" >
+        <
+        h3 > Zonful Energy, Solar power to the people < /h3> <
+        span > If you are interested in going solar, we have every solar solution
+        for you < i className = "material-icons" > call_made < /i></span >
+        <
+        /div> <
+        /div> <
+        /div> <
+        /React.Fragment>
     )
 }
 
