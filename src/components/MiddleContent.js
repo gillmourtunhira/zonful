782d1814898eb7069c4ffffa6bc2454bb7838f67
gillmourtunhira <gillmourtunhira@gmail.com>
2020-12-08 11:02:53 +0200

    changes to the middle tier

--- src/components/MiddleContent.js
+++ src/components/MiddleContent.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import zimbabwe from '../img/zimbabwe.png'
+import review from '../img/review.png'
+import retailer from '../img/retailer.png'
+import sunking from '../img/sunking.png'
 
 function MiddleContent(){
     return(
-        <div>
-            <h1>Hello from Zonful Energy!</h1>
+        <React.Fragment>
+        <div className="middle__header">
+            <h2>Why Most Zimbabweans Prefer Zonful Energy?</h2>
         </div>
+        <div className="blobs">
+            <div>
+                <img src={zimbabwe} alt=""/>
+                <article className="blobs--title">
+                    <h4>Zimbabwe's Largest</h4>
+                    <h6>Solar Company</h6>
+                </article>
+            </div>
+            <div>
+            <img src={review} alt=""/>
+                <article className="blobs--title">
+                    <h4></h4>
+                    <h6>Only Solar Provider With <b>200k </b> + Five Star Reviews</h6>
+                </article>
+            </div>
+            <div>
+                <img src={retailer} alt=""/>
+                <article className="blobs--title">
+                    <h4>Most Trusted</h4>
+                    <h6>Solar Retailer</h6>
+                </article>
+            </div>
+            <div>
+            <img src={sunking} alt=""/>
+                <article className="blobs--title">
+                    <h4>Sunking</h4>
+                    <h6>Greenlight Partner</h6>
+                </article>
+            </div>
+        </div>
+        </React.Fragment>
     )
 }
 
